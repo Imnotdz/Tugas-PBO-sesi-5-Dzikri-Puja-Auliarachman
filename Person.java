0b2f302bef0e7544a7a6ea62d694c952ec20ac09
Imnotdz <114376083+Imnotdz@users.noreply.github.com>
2022-10-24 22:17:01 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO5;
+
+/**
+ *
+ * @author dzpa1
+ */
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getAddress(){
+        return this.address;
+    }
+
+    public void setName(String address){
+        this.address = address;
+    }
+
+    public String toString(){
+        return
+        "Nama: " + getName() +
+        "\nAlamat: " + getAddress();
+    }
+}
